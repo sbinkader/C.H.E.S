1edaadda78a7bebd861308905bcc127c3e87f1bf
Safran Bin Kader <118633138+sbinkader@users.noreply.github.com>
2025-02-21 23:52:12 -0600

    Changes to gamePage

---
+++ C.H.E.S/src/gamepage.jsx
@@ -0,0 +1,65 @@
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+
+function GamePage() {
+    return (
+        <Card
+      shadow="sm"
+      padding="xl"
+      component="a"
+      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      target="_blank"
+      style={{width: "200px", height: "100px"}}
+    >
+      <Card.Section>
+        <Image
+          src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+          h={160}
+          w={150}
+          alt="No way!"
+        />
+      </Card.Section>
+
+      <Text fw={500} size="lg" mt="md">
+        You&apos;ve won a million dollars in cash!
+      </Text>
+
+      <Text mt="xs" c="dimmed" size="sm">
+        Please click anywhere on this card to claim your reward, this is not a fraud, trust us
+      </Text>
+    </Card>
+    );
+}
+
+
+
+function GameCard() {
+  return (
+    <>
+    <Card shadow="lg" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
+    </>
+  );
+}
+
+export default GamePage;
